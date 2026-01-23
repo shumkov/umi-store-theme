c29cc351d80580ac7182220062ce4a118ef7ae49
Ivan Shumkov <ivanshumkov@gmail.com>
2026-01-23 18:26:16 +0700

    fix: scroll lightbox to center on clicked point

--- assets/product-modal.js
+++ assets/product-modal.js
@@ -4,13 +4,15 @@ if (!customElements.get('product-modal')) {
     class ProductModal extends ModalDialog {
       constructor() {
         super();
+        this.clickPosition = null;
       }
 
       hide() {
         super.hide();
       }
 
-      show(opener) {
+      show(opener, clickPosition = null) {
+        this.clickPosition = clickPosition;
         super.show(opener);
         this.showActiveMedia();
       }
@@ -25,10 +27,29 @@ if (!customElements.get('product-modal')) {
         const activeMediaTemplate = activeMedia.querySelector('template');
         const activeMediaContent = activeMediaTemplate ? activeMediaTemplate.content : null;
         activeMedia.classList.add('active');
-        activeMedia.scrollIntoView();
 
         const container = this.querySelector('[role="document"]');
-        container.scrollLeft = (activeMedia.width - container.clientWidth) / 2;
+
+        // Scroll to clicked position if available
+        if (this.clickPosition && activeMedia.tagName === 'IMG') {
+          // Wait for image to be visible and get its dimensions
+          requestAnimationFrame(() => {
+            const imgWidth = activeMedia.offsetWidth;
+            const imgHeight = activeMedia.offsetHeight;
+            const containerWidth = container.clientWidth;
+            const containerHeight = container.clientHeight;
+
+            // Calculate scroll position to center the clicked point
+            const scrollX = (imgWidth * this.clickPosition.clickX) - (containerWidth / 2);
+            const scrollY = (imgHeight * this.clickPosition.clickY) - (containerHeight / 2);
+
+            container.scrollLeft = Math.max(0, scrollX);
+            container.scrollTop = Math.max(0, scrollY);
+          });
+        } else {
+          activeMedia.scrollIntoView();
+          container.scrollLeft = (activeMedia.width - container.clientWidth) / 2;
+        }
 
         if (
           activeMedia.nodeName == 'DEFERRED-MEDIA' &&
